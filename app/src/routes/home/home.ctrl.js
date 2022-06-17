c5b24669e62bd49fdd2af8693305915fcc91d725
danpung2 <danpung_2@naver.com>
2022-06-17 18:06:22 +0900

    CREATE login, sign up page

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -8,12 +9,15 @@ const output = {
     },
     login: (req, res) => {
         res.render("home/login");
+    },
+    register: (req, res) => {   
+        res.render("home/register");
     }
 };
 
 const process = {
     login: (req, res) => {
-        const user = new User(req, body);
+        const user = new User(req);
         const response = user.login();
         return res.json(response);
     },
